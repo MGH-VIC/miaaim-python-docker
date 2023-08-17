c23bac6751988a6ac1c8d0e222b02fe3b939360c
JoshuaHess12 <joshmhess12@gmail.com>
2023-08-17 10:32:50 -0400

    bug fix pip install centrosome

--- install_env.sh
+++ install_env.sh
@@ -14,9 +14,11 @@ conda install -c -y conda-forge pathlib
 conda install -c -y conda-forge uncertainties
 conda install -c -y conda-forge pyyaml
 conda install -c conda-forge tensorflow
+conda install -c conda-forge squidpy
 # conda install -c -y conda-forge tensorflow-probability
 conda install -c -y conda-forge umap-learn
 # conda install -c bioconda cellprofiler
+# conda install -c conda-forge leidenalg
 conda install -c conda-forge spyder
 conda install -y dask
 conda install -y ipykernel
